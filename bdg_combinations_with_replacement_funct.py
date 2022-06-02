43cbc783499959c5f8a2be012d16bfbde0aae543
bdg92 <badalgottlieb777@gmail.com>
2022-06-02 15:17:50 +0530

    Add files via upload

---
+++ bdg_combinations_with_replacement_funct.py
@@ -0,0 +1,21 @@
+## Let's try making a function called bdg_combination which will take argument as a list and length of bdg_combination
+## And will return the possible combinations of the elements of the list.
+
+def bdg_combination (l,g):
+    n = len(l)
+    nl = []
+    el = []
+    loop = 0
+    while loop < n:
+        for x in range(0,n):
+            if x< loop:
+                continue
+            el = (l[loop],l[x])
+            nl.append(el)
+        loop +=1
+    print(nl)
+    print("No. of combinations which can be made from this set of data is:",len(nl))
+
+l = [1,2,3,4,5,6]
+g =2
+bdg_combination(l,g)
